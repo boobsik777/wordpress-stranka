80caceb087fb692d902b243200eb75e5bec258da
luk4sko <lukasbrtko1@gmail.com>
2025-10-15 23:18:16 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Agro Contractor</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="navbar">
+    <img src="logo.jpg" alt="Agro Contractor Logo">
+    
+    <div class="menu">
+        <!-- Dropdown for Uprawa -->
+        <div class="dropdown">
+            <button class="dropbtn">Uprawa</button>
+            <div class="dropdown-content">
+                <a href="#"><img src="obrazok1.png" alt="Option 1"> Siew buraka</a>
+                <a href="#"><img src="obrazok2.png" alt="Option 2"> Siew zbóż</a>
+                <a href="#"><img src="obrazok3.png" alt="Option 3"> Siew rzepaku</a>
+                <a href="#"><img src="obrazok4.png" alt="Option 4"> Siew kukurydzy</a>
+            </div>
+        </div>
+
+        <!-- Other menu items -->
+        <a href="#">Siew</a>
+        <a href="#">Zbiór</a>
+        <a href="#">Inne usługi</a>
+        <a href="#">Blog</a>
+        <a href="#">Kontakt</a>
+    </div>
+
+    <div class="contact">
+        <span class="contact-number">+48 123 456 789</span>
+        <a href="#" class="contact-button">Napisz do nas</a>
+    </div>
+</div>
+
+</body>
+</html>
