6131a990f0b923504ee55ede58a8c0b6a5226b15
phyrosova <p.hyrosova@gmail.com>
2025-10-17 12:26:46 +0200

    footer

---
+++ footer.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-9">
+        <meta name="viewport">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title>Footer</title>
+    </head>
+    <body>
+        <div class="container-fluid" style="background-color: #D9D9D9;">
+            <div class="container">
+                <br>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <br><br><br><br><br>
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/logo-1.png">
+                        <br><br><br><br>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h5><b>KONTAKT</b></h5>
+                                <br>
+                                <p>
+                                    Magazynowa 2, 86-200 Chełmno
+                                </p>
+                                <p>
+                                    +48 123 456 789
+                                </p>
+                                <p>
+                                    kontakt@agrocontractor.pl
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <h5><b>INFORMACJE</b></h5>
+                                <br>
+                                <p>
+                                    Klauzula informacyjna
+                                </p>
+                                <p>
+                                    Polityka prywatności
+                                </p>
+                            </div>
+                        </div>
+                        <br><br>
+                        <h5><b>ODWIEDŹ NAS</b></h5>
+                        <br>
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/vector.png">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/subtract-1.png">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/vector-1.png">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/vector-2.png">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/vector-3.png">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <br><br><br><br>
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-63.png">
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="well" style="background-color: #fff;">
+                            <h3>Zainteresowała Cię nasza oferta?</h3>
+                            <br>
+                            <p>
+                                Wypełnij poniższy formularz, a nasz doradca skontaktuje się z Tobą i odpowie na pytania.
+                            </p>
+                            <br>
+                            <form action="" method="post">
+                                <!-- 1 -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <h6>Wybierz rodzaj usługi*</h6>
+                                        <select id="wybierz" name="wybierz" style="width: 100%; height: 43px; padding: 12px;">
+                                            <option value="siew">Siew</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <h6>Twoje imię i nazwisko*</h6>
+                                        <input type="text" id="imie" placeholder="Wpisz tutaj" name="imie" style="width: 100%; height: 43px; padding: 12px;">
+                                    </div>
+                                </div>
+                                <br>
+                                <!-- 1 -->
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <h6>Twój numer telefonu*</h6>
+                                        <input type="text" id="telefon" placeholder="Wpisz tutaj" name="telefon" style="width: 100%; height: 43px; padding: 12px;">
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <h6>Twój adres e-mail*</h6>
+                                        <input type="email" id="email" placeholder="Wpisz tutaj" name="email" style="width: 100%; height: 43px; padding: 12px;">
+                                    </div>
+                                </div>
+                                <br>
+                                <h6>Twoja wiadomość*</h6>
+                                <textarea class="form-control" rows="5" id="comment" placeholder="Wpisz tutaj" style="width: 100%; resize: none;"></textarea>
+                                <br>
+                                <p><input type="checkbox" name="checkbox"> Oświadczam, że zapoznałem się z treścią polityki prywatności i akceptuję zasady przetwarzania moich danych osobowych*</p>
+                                <br>
+                                <button type="submit" class="btn btn-light" style="background-color: #809A1B; color: #fff; width: 100%; border-radius: 50px;">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
