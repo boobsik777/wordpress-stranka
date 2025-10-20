d152fb468eef9c0b7236bf0d22632d023c4c706e
phyrosova <p.hyrosova@gmail.com>
2025-10-20 10:55:49 +0200

    upraveny default

--- default.php
+++ default.php
@@ -15,86 +15,94 @@
     </head>
     <body>
         <div class="container">
-            <button type="button" class="btn btn-light">Strona główna</button>
-            <h1 class="title">Polityka prywatności</h1>
+            <h1 style="color: #383B3F;">Polityka prywatności</h1>
             <br><br><br>
             <!-- 1 -->
-            <h4>Lorem ipsum dolor sit amet</h4>
+            <h3 style="color: #383B3F;">Lorem ipsum dolor sit amet</h3>
             <br>
-            <p>Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
-                Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
-                Objectively restore leading-edge growth strategies without standardized networks. 
-                Assertively matrix performance based services and B2C data. 
-                Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
-            </p>
-            <br>
-            <p>
-                Collaboratively orchestrate client-focused functionalities before viral materials. 
-                Dynamically benchmark leading-edge systems for resource sucking value. 
-                Rapidiously transition installed base products with effective e-services. 
-                Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
-                Enthusiastically incubate intuitive value rather than corporate deliverables.
-            </p>
-            <br>
-            <p>
-                Professionally network revolutionary networks without worldwide portals. 
-                Synergistically maximize enterprise e-services through just in time partnerships. 
-                Holisticly envisioneer fully researched technology via real-time channels. 
-                Conveniently implement distinctive communities with real-time benefits. 
-                Continually transition optimal niche markets with go forward services.
-            </p>
+            <div style="font-size: 16px; color: #60646A;">
+                <p>
+                    Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
+                    Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
+                    Objectively restore leading-edge growth strategies without standardized networks. 
+                    Assertively matrix performance based services and B2C data. 
+                    Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
+                </p>
+                <br>
+                <p>
+                    Collaboratively orchestrate client-focused functionalities before viral materials. 
+                    Dynamically benchmark leading-edge systems for resource sucking value. 
+                    Rapidiously transition installed base products with effective e-services. 
+                    Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
+                    Enthusiastically incubate intuitive value rather than corporate deliverables.
+                </p>
+                <br>
+                <p>
+                    Professionally network revolutionary networks without worldwide portals. 
+                    Synergistically maximize enterprise e-services through just in time partnerships. 
+                    Holisticly envisioneer fully researched technology via real-time channels. 
+                    Conveniently implement distinctive communities with real-time benefits. 
+                    Continually transition optimal niche markets with go forward services.
+                </p>
+            </div>
             <br><br><br>
             <!-- 2 -->
-            <h4>Lorem ipsum dolor sit amet</h4>
+            <h3 style="color: #383B3F;">Lorem ipsum dolor sit amet</h3>
             <br>
-            <p>Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
-                Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
-                Objectively restore leading-edge growth strategies without standardized networks. 
-                Assertively matrix performance based services and B2C data. 
-                Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
-            </p>
-            <br>
-            <p>
-                Collaboratively orchestrate client-focused functionalities before viral materials. 
-                Dynamically benchmark leading-edge systems for resource sucking value. 
-                Rapidiously transition installed base products with effective e-services. 
-                Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
-                Enthusiastically incubate intuitive value rather than corporate deliverables.
-            </p>
-            <br>
-            <p>
-                Professionally network revolutionary networks without worldwide portals. 
-                Synergistically maximize enterprise e-services through just in time partnerships. 
-                Holisticly envisioneer fully researched technology via real-time channels. 
-                Conveniently implement distinctive communities with real-time benefits. 
-                Continually transition optimal niche markets with go forward services.
-            </p>
+            <div style="font-size: 16px; color: #60646A;">
+                <p>
+                    Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
+                    Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
+                    Objectively restore leading-edge growth strategies without standardized networks. 
+                    Assertively matrix performance based services and B2C data. 
+                    Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
+                </p>
+                <br>
+                <p>
+                    Collaboratively orchestrate client-focused functionalities before viral materials. 
+                    Dynamically benchmark leading-edge systems for resource sucking value. 
+                    Rapidiously transition installed base products with effective e-services. 
+                    Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
+                    Enthusiastically incubate intuitive value rather than corporate deliverables.
+                </p>
+                <br>
+                <p>
+                    Professionally network revolutionary networks without worldwide portals. 
+                    Synergistically maximize enterprise e-services through just in time partnerships. 
+                    Holisticly envisioneer fully researched technology via real-time channels. 
+                    Conveniently implement distinctive communities with real-time benefits. 
+                    Continually transition optimal niche markets with go forward services.
+                </p>
+            </div>
             <br><br><br>
             <!-- 3 -->
-            <h4>Lorem ipsum dolor sit amet</h4>
+            <h3 style="color: #383B3F;">Lorem ipsum dolor sit amet</h3>
             <br>
-            <p>Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
-                Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
-                Objectively restore leading-edge growth strategies without standardized networks. 
-                Assertively matrix performance based services and B2C data. 
-                Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
-            </p>
-            <br>
-            <p>
-                Collaboratively orchestrate client-focused functionalities before viral materials. 
-                Dynamically benchmark leading-edge systems for resource sucking value. 
-                Rapidiously transition installed base products with effective e-services. 
-                Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
-                Enthusiastically incubate intuitive value rather than corporate deliverables.
-            </p>
-            <br>
-            <p>
-                Professionally network revolutionary networks without worldwide portals. 
-                Synergistically maximize enterprise e-services through just in time partnerships. 
-                Holisticly envisioneer fully researched technology via real-time channels. 
-                Conveniently implement distinctive communities with real-time benefits. 
-                Continually transition optimal niche markets with go forward services.
-            </p>
+            <div style="font-size: 16px; color: #60646A;">
+                <p>
+                    Distinctively reconceptualize best-of-breed outsourcing via top-line bandwidth. 
+                    Objectively pursue 24/365 infomediaries via cutting-edge scenarios. 
+                    Objectively restore leading-edge growth strategies without standardized networks. 
+                    Assertively matrix performance based services and B2C data. 
+                    Proactively expedite clicks-and-mortar methodologies with integrated collaboration and idea-sharing.
+                </p>
+                <br>
+                <p>
+                    Collaboratively orchestrate client-focused functionalities before viral materials. 
+                    Dynamically benchmark leading-edge systems for resource sucking value. 
+                    Rapidiously transition installed base products with effective e-services. 
+                    Dynamically harness end-to-end deliverables rather than low-risk high-yield intellectual capital. 
+                    Enthusiastically incubate intuitive value rather than corporate deliverables.
+                </p>
+                <br>
+                <p>
+                    Professionally network revolutionary networks without worldwide portals. 
+                    Synergistically maximize enterprise e-services through just in time partnerships. 
+                    Holisticly envisioneer fully researched technology via real-time channels. 
+                    Conveniently implement distinctive communities with real-time benefits. 
+                    Continually transition optimal niche markets with go forward services.
+                </p>
+            </div>
             <br><br><br>
         </div>
     </body>
