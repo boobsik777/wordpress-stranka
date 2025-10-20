1bfeff22b5611d2b3e6fed99b853a324539e46bb
phyrosova <p.hyrosova@gmail.com>
2025-10-20 11:17:17 +0200

    upravena kategoria

---
+++ kategoria.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-9">
+        <meta name="viewport">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <title>Siew</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1 style="color: #383B3F;">Siew</h1>
+            <br><br><br>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="well" style="background-color: #FFFFFF; text-align: center; border: none; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);">
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/group-4-1.png">
+                        <br><br>
+                        <p style="color: #383B3F; font-size: 16px;">Orka</p>
+                        <button class="arrow-btn"><img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-16.png" alt="right arrow"></button>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well" style="background-color: #FFFFFF; text-align: center; border: none; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);">
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/group-4.png">
+                        <br><br>
+                        <p style="color: #383B3F; font-size: 16px;">Talerzowanie</p>
+                        <button class="arrow-btn"><img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-16.png" alt="right arrow"></button>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="well" style="background-color: #FFFFFF; text-align: center; border: none; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);">
+                        <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-5.png">
+                        <br><br>
+                        <p style="color: #383B3F; font-size: 16px;">Głęboszowanie</p>
+                        <button class="arrow-btn"><img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-16.png" alt="right arrow"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br>
+        <div class="container-fluid" style="background-color: #809A1B; color: #FFFFFF;">
+            <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/subtract.png" style="width: 100%;">
+            <div class="container">
+                <br><br><br>
+                <p style="font-size: 12px;">FAQ</p>
+                <div style="display: flex; justify-content: space-between; align-items: center;">
+                    <h2>Jak wynająć?</h2>
+                    <button type="button" class="btn btn-light" style="color: #383B3F; font-size: 16px;">Skontaktuj się</button>
+                </div>
+                <br><br><br><br>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="thumbnail" style="padding: 25px; background-color: #809A1B; text-align: center; border-radius: 10px;">
+                            <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-38-2.png">
+                            <br><br>
+                            <h3 style="font-size: 22px;">01. Lorem ipsum dolor sit amet</h3>
+                            <br>
+                            <p style="font-size: 16px;">
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="thumbnail" style="padding: 25px; background-color: #809A1B; text-align: center; border-radius: 10px;">
+                            <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-38-1.png">
+                            <br><br>
+                            <h3 style="font-size: 22px;">02. Lorem ipsum dolor sit amet</h3>
+                            <br>
+                            <p style="font-size: 16px;">
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="thumbnail" style="padding: 25px; background-color: #809A1B; text-align: center; border-radius: 10px;">
+                            <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/frame-38.png">
+                            <br><br>
+                            <h3 style="font-size: 22px;">03. Lorem ipsum dolor sit amet</h3>
+                            <br>
+                            <p style="font-size: 16px;">
+                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <br><br><br><br>
+            </div>
+            <img src="https://group1422.wordpress.com/wp-content/uploads/2025/10/cat.png" style="width: 100%;">
+        </div>
+    </body>
+</html>
